7079b852f4d0872c70ba1ab2cd60c18bb9c2d160
mzainmumtaz <mzainmumtaz99@gmail.com>
2025-04-21 08:08:43 +0500

    Add new feature or fix bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,51 @@
 import express from "express";
+import serverless from "serverless-http";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import {  registerRouter } from "./routes/registerRouter";
-import { loginRouter } from "./routes/loginRouter";
-import path from "path";
+import {  registerRouter } from "../src/routes/registerRouter";
+
+
+import { loginRouter } from "../src/routes/loginRouter";
 import {v2 as cloudinary} from "cloudinary";
 
-
+// Initialize app
 const app = express();
-const PORT = process.env.PORT;
 app.use(express.json());
 
+// Database connection
 mongoose
   .connect(process.env.MONGO_CONNECTON_STRING as string)
   .then(() => console.log("database connected successfully"));
 
+// Middleware
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL as string,
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL as string,
+    origin: process.env.FRONTEND_URLNETLIFY  ||  "http://localhost:5173",
     credentials: true,
   })
 );
+
+// Cloudinary config
 cloudinary.config({ 
   cloud_name: "zainmughal999", 
   api_key:"744766614756274", 
   api_secret: "F5uKFc-wILFbT2CW44eUJzDV8o8"
 });
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 app.use(cookieParser());
-
-
-
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", registerRouter);
 app.use("/api", loginRouter);
-// app.use("/api", addJobRouter);
-// app.use("/api", adminRegisterRouter);
-// app.use("/api", adminLoginRouter);
-
-app.listen(PORT, () => {
-  console.log(`app is listeining  at ${PORT}`);
-});
+// Export the serverless handler
+const handler = serverless(app);
+console.log("handler",handler);
+export { handler };
